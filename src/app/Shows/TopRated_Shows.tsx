03de9f931294da9e7754a3cea54779f8cb3a8787
sravan kumar <sravanpatel0017@gmail.com>
2023-11-21 21:18:18 +0530

    Top rated slider

---
+++ src/app/Shows/TopRated_Shows.tsx
@@ -0,0 +1,23 @@
+import { TopRated } from "../utils/request";
+
+export default async function TopRated_Shows() {
+    let ShowsData = await TopRated();
+  return (
+    <div className="bg-black overflow-hidden">
+      <h1 className="text-red-500 text-lg md:text-xl ml-6 font-semibold">Top Rating Shows</h1>
+    <div className="w-screen flex md:gap-6 gap-1 Trending ml-1 mt-2">
+      {
+        ShowsData.map((item:any,id:number)=>{
+          return(
+            <div key={id}>
+                <div className="w-40 h-40 md:w-60 md:h-60">
+                <img className="w-40 h-40 md:h-60 md:w-60 rounded-md contrast-75 hover:contrast-125 duration:200" src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`} alt="images"/>
+                </div>
+            </div>
+          )  
+        })
+      }
+    </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Slider from "./Slider/Slider";
 import  Trending_Movies  from "./Movies/Trending_Movies";
 import  Popular_Movies  from "./Movies/Popular_Movies";
 import  Trending_Shows  from "./Shows/Trending_Shows";
+import  TopRated_Shows  from "./Shows/TopRated_Shows";
 
 export default async function Home() {
   return (
@@ -10,6 +11,7 @@ export default async function Home() {
       <Trending_Movies/>   
       <Popular_Movies/>   
       <Trending_Shows/>   
+      <TopRated_Shows/>   
     </div>
   )
 }
